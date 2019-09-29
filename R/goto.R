56af821c44ae9fc8afbead8d13384d468cc28e4f
William Doane <wdoane@ida.org>
2019-09-29 11:25:32 -0400

    Add error checking

--- R/goto.R
+++ R/goto.R
@@ -12,16 +12,26 @@ goto_reference <-
       return(invisible(NULL))
     }
 
-    text <- read_utf8(issue)
+    text <- rstudioapi::getSourceEditorContext()[["contents"]]
 
     ref <- which(grepl("^While viewing: ", text))
+
+    if (length(ref) == 0) {
+      message(issue, " doesn't appear to contain a source file reference.")
+      return(invisible(NULL))
+    }
 
     ref <- gsub("^While viewing: ", "", text[ref])
 
     filename <- unlist(strsplit(ref, "@"))[1]
     line <- unlist(strsplit(ref, "@"))[2]
 
-    rstudioapi::navigateToFile(filename, line, column = 1)
+    if (!file.exists(here::here(filename))) {
+      message(filename, " doesn't appear to exist.")
+      return(invisible(NULL))
+    }
+
+    rstudioapi::navigateToFile(here::here(filename), line, column = 1)
 
     invisible(NULL)
   }
