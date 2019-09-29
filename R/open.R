6819829955a84f1ff58b074350084f4fd401927c
William Doane <wdoane@ida.org>
2019-09-29 00:14:14 -0400

    Clean up

--- R/open.R
+++ R/open.R
@@ -1,33 +1,33 @@
 open_issue <-
   function(title = rstudioapi::showPrompt("Open Issue", "Issue Title")) {
-  if (is.null(title)) {
-    message("You must provide a title in order to open an issue.")
-    return(invisible(NULL))
-  }
+    if (is.null(title)) {
+      message("You must provide a title in order to open an issue.")
+      return(invisible(NULL))
+    }
 
-dir_setup()
+    dir_setup()
 
-hash <- issue_hash()
-filename <- here::here("issuer", "open", paste0(hash, ".md"))
+    hash <- issue_hash()
+    filename <- here::here("issuer", "open", paste0(hash, ".md"))
 
-issue <-
-  paste0(
-    title,
-    "\nOpened by: ",
-    username(),
-    "\nWhile viewing: ",
-    basename(rstudioapi::getSourceEditorContext()[["path"]]),
-    "@",
-    rstudioapi::getSourceEditorContext()$selection[[1]]$range$start[1],
-    "\n\nDetail:\n\n"
-  )
+    issue <-
+      paste0(
+        title,
+        "\nOpened by: ",
+        username(),
+        "\nWhile viewing: ",
+        basename(rstudioapi::getSourceEditorContext()[["path"]]),
+        "@",
+        rstudioapi::getSourceEditorContext()$selection[[1]]$range$start[1],
+        "\n\nDetail:\n\n"
+      )
 
-write_utf8(issue, filename)
+    write_utf8(issue, filename)
 
-list_issues()
+    list_issues()
 
-rstudioapi::navigateToFile(filename, line = 7, column = 1)
+    rstudioapi::navigateToFile(filename, line = 7, column = 1)
 
-return(invisible(filename))
+    return(invisible(filename))
 
   }
